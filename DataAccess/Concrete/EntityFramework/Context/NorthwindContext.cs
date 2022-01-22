67bd41a375e414bc3a7c5f1cb598dde301acc5f4
sahranurer <sahranurerr@gmail.com>
2022-01-22 12:44:29 +0300

    DataAccess Created

---
+++ DataAccess/Concrete/EntityFramework/Context/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-HE88LIR; database=Northwind; Trusted_Connection=true");
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
